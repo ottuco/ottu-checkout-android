4c90f917498589562214f79d1395df91f681094e
Serhii.Slobodianiuk <sslobodianiuk@plexteq.com>
2024-03-14 12:30:26 +0200

    Added dark mode for the payment methods list

--- Ottu/src/main/java/Ottu/ui/payment_methods/PaymentMethodsAdapter.java
+++ Ottu/src/main/java/Ottu/ui/payment_methods/PaymentMethodsAdapter.java
@@ -71,15 +71,14 @@ public class PaymentMethodsAdapter extends RecyclerView.Adapter<PaymentMethodsAd
         public int getIconByType(String type) {
             switch (type) {
                 case "1":
-                    return R.drawable.card_visa;
+                    return R.drawable.ic_google_pay;
                 case "2":
-                    return R.drawable.card_visa;
                 case "3":
-                    return R.drawable.card_visa;
+                    return R.drawable.ic_stc_pay;
                 case "4":
-                    return R.drawable.card_visa;
+                    return R.drawable.ic_saved_card;
                 case "5":
-                    return R.drawable.card_visa;
+                    return R.drawable.ic_knet;
                 default:
                     return R.drawable.card_visa;
             }
